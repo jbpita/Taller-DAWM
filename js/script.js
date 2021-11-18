ea9d88342558239eec946587f4850ab448847220
jbpita <jbpita@espol.edu.ec>
2021-11-18 12:34:43 -0500

    Second Commit

--- js/script.js
+++ js/script.js
@@ -13,18 +13,41 @@
   3. Agregue el nuevo valor al inicio del elemento div con identificador "list"
  */
 
-const URL = 'http://ghibliapi.herokuapp.com/'
-const RECURSO = "films"
-let plantilla = `
-<div class="product product-container">
-  <div class="inner-product">
-    <div class="figure-image">
-      <a href="single.html"><img src="{{image}}" alt="{{title}}"></a>
-    </div>
-    <h3 class="product-title"><a href="#">{{title}}</a></h3>
-    <small class="price">{{original_title}}</small>
-    <p>{{description}}</p>
-    <a href="cart.html" class="button">Watch it</a>
-    <a href="#" class="button muted">Read Details</a>
-  </div>
-</div> <!-- .product -->`
+const URL = 'http://ghibliapi.herokuapp.com/';
+const RECURSO = "films";
+
+fetch(URL+RECURSO)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+    json.forEach(element => {
+      console.log(element);
+      let title = element.title;
+      let image = element.image;
+      let description = element.description.substring(0,70) ;
+      let original_title = element.original_title;
+
+      console.log(image)
+      console.log("titulo: " ,title)
+      const ele = document.getElementById("list");
+
+      let plantilla = `
+          <div class="product product-container">
+            <div class="inner-product">
+              <div class="figure-image">
+                <a href="single.html"><img src=${image}" alt="${title}"></a>
+              </div>
+              <h3 class="product-title"><a href="#">${title}$</a></h3>
+              <small class="price">${original_title}</small>
+              <p>${description}</p>
+              <a href="cart.html" class="button">Watch it</a>
+              <a href="#" class="button muted">Read Details</a>
+            </div>
+          </div> <!-- .product -->
+        `;
+      let aux = ele.innerHTML;
+      
+      ele.innerHTML = plantilla + aux;
+
+    });
+  });
